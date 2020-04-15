0192d33e55386dd07ae60b4f291e94673b3d4af3
Andres Zapata <azo_67@hotmail.es>
2020-04-15 09:56:14 -0500

    First material

---
+++ bastion-userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-add-repository ppa:ansible/ansible -y && sudo apt update
+sudo apt install ansible python3-pip python-pip -y
+# sudo pip install boto
+git clone https://github.com/gaearaz/DevOps-RampUp-Res.git /home/ubuntu/
