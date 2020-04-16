7d2dcf58369e4a5493ceb1c894872307620a9e05
Andres Zapata <azo_67@hotmail.es>
2020-04-16 08:20:10 -0500

    creation of some important files to provision (not complete yet)

--- bastion-userdata.sh
+++ bastion-userdata.sh
@@ -2,4 +2,4 @@
 sudo apt-add-repository ppa:ansible/ansible -y && sudo apt update
 sudo apt install ansible python3-pip python-pip -y
 # sudo pip install boto
-git clone https://github.com/gaearaz/DevOps-RampUp-Res.git /home/ubuntu/rampup/
+git -C /home/ubuntu/ clone https://github.com/gaearaz/DevOps-RampUp-Res.git
